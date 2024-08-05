61d9b8cd1a9007811a85cf1bb7951c563998654e
Andrey Leone <andreeyleonne92@gmail.com>
2024-08-05 08:47:27 -0300

    corrigindo erros de sintax

--- SQL/ddl.sql
+++ SQL/ddl.sql
@@ -5,7 +5,7 @@ create database LojaPet;
 \c LojaPet
 
 create table tb_enderecos (
-    id_endereco serial primary key,
+    id_endereco int primary key,
     cep varchar(9) not null,
     rua varchar(100) not null,
     bairro varchar(100) not null,
@@ -16,7 +16,7 @@ create table tb_enderecos (
 );
 
 create table tb_produtos (
-    id_produtos serial primary key,
+    id_produtos int primary key,
     nome_produto varchar(100) not null,
     descricao varchar(255) not null,
     quantidade int not null,
@@ -25,13 +25,14 @@ create table tb_produtos (
 );
 
 create table tb_usuarios (
-    id_usuario serial primary key,
+    id_usuario int primary key,
     nome varchar(50) not null,
     sobrenome varchar(50) not null,
     cpf varchar(11) unique not null,
     data_cadastro timestamp defaut NOW(),
     id_contato int,
     id_endereco int
+    id_credencial int,
 
     foreign key (id_contato) references tb_contato (id_contato),
     foreign key (id_endereco) references tb_enderecos (id_endereco),
@@ -39,39 +40,34 @@ create table tb_usuarios (
 );
 
 create table tb_credenciais (
-    id_credencial serial primary key,
+    id_credencial int primary key,
     id_usuario int unique,
     email varchar(100) not null,
-    senha varchar(255) not null,
-
-    foreign key (id_usuario) references tb_usuarios (id_usuario)   
+    senha varchar(255) not null, 
 );
 
 create table tb_contato (
-    id_contato serial primary key,
+    id_contato int primary key,
     celular varchar(14) unique not null,
     email varchar(100) unique,
-
-    foreign key (email) references tb_credenciais (email)    
  );
 
 create table tb_pedidos (
-    id_pedidos serial primary key,
-    id_usuario serial unique,
-    data_pedido timestamp defaut NOW,
+    id_pedidos int primary key,
+    id_usuario int unique,
+    data_pedido timestamp defaut NOW(),
     id_produtos int not null,
     quantidade int(100),
-    preco numeric(5,2) not null
+    preco numeric(5,2) not null,
 
     foreign key (id_usuario) references tb_usuarios(id_usuario),
     foreign key (id_produtos) references tb_produtos(id_produtos)
 );
 
-create table tb_fornecedores {
-    id_fornecedor serial primary key,
+create table tb_fornecedores (
+    id_fornecedor int primary key,
     razao varchar(64) not null,
     cnpj varchar (14) not null,
     telefone varchar(12) not null,
     email varchar(128) not null
-}
-
+)
